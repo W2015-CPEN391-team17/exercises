273ad45c4a4837c31cea10afcc708c6385b034ab
Nicholas Mulvenna <mulvenna@linux.com>
2016-01-12 16:32:18 -0800

    files for bsp and edited some hardware

--- exercise1/exercise1.vhd
+++ exercise1/exercise1.vhd
@@ -1,7 +1,7 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all; 
-USE ieee.std_logic_arith.all; 
-USE ieee.std_logic_unsigned.all; 
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
+USE ieee.std_logic_unsigned.all;
 
 ENTITY exercise1 IS
 
---
+++ exercise1/software/exercise1/obj/default/exercise1.d
@@ -0,0 +1,1 @@
+obj/default/exercise1.o: exercise1.c
