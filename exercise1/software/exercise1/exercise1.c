0129e0a236ab9f5b7787066f942896a4ab475307
Nicholas Mulvenna <mulvenna@linux.com>
2016-01-12 17:21:46 -0800

    wip printing current key to lcd

--- exercise1/software/exercise1/exercise1.c
+++ exercise1/software/exercise1/exercise1.c
@@ -8,32 +8,71 @@
  */
 
 #include <stdio.h>
+#include "altera_up_avalon_character_lcd.h"
 
 // upper four bits of KEYS_ACTIVE_HIGH are always zero (see VHDL)
 #define KEYS_ACTIVE_HIGH (volatile char *) 0x00001000
 #define SWITCHES (volatile char*) 0x00001020
 #define LEDS (char *) 0x00001010
 
+#define LCD_LINE_LENGTH 16
+
+#define KEY0_MASK 0x01
+#define KEY1_MASK 0x02
+#define KEY2_MASK 0x03
+#define KEY3_MASK 0x04
+
+#define KEY_STRING "KEY "
+#define KEY_STRING_EMPTY "     "
+
 int main()
 {
   printf("Exercise 1.1 - Press all four keys simultaneously to reset\n");
+  unsigned int button_held_down = 0;
+  unsigned int button_previously_held_down = 1;
+  unsigned int key_number = 0;
+  char key_number_string[LCD_LINE_LENGTH] = "";
 
-  char number_of_button_presses = 0;
-  char button_previously_held_down = 0;
+  alt_up_character_lcd_dev * char_lcd_dev;
+
+  char_lcd_dev = alt_up_character_lcd_open_dev("/dev/character_lcd_0");
+  if (char_lcd_dev == NULL) {
+	  printf("Error could not open character LCD device\n");
+  } else {
+	  printf("Opened character LCD device\n");
+  }
+
+  alt_up_character_lcd_init(char_lcd_dev);
 
   while (1) {
-	  if (*KEYS_ACTIVE_HIGH) {
-		  if (!button_previously_held_down) {
-			  button_previously_held_down = 1;
+	  if (*KEYS_ACTIVE_HIGH && !button_previously_held_down) {
+		  button_held_down = 1;
+		  if (KEY0_MASK & *KEYS_ACTIVE_HIGH) {
+			  key_number = 0;
+		  } else if (KEY1_MASK & *KEYS_ACTIVE_HIGH) {
+			  key_number = 1;
+		  } else if (KEY2_MASK & *KEYS_ACTIVE_HIGH) {
+			  key_number = 2;
+		  } else if (KEY3_MASK & *KEYS_ACTIVE_HIGH) {
+			  key_number = 3;
 		  }
 	  } else {
-		  if (button_previously_held_down) {
-			  button_previously_held_down = 0;
-			  number_of_button_presses++;
-			  printf("%d\n", number_of_button_presses);
+		  button_held_down = 0;
+	  }
+
+	  if (button_held_down != button_previously_held_down) {
+		  if (button_held_down) {
+			  sprintf(key_number_string, "%u", key_number);
+			  alt_up_character_lcd_set_cursor_pos(char_lcd_dev,0,0);
+			  alt_up_character_lcd_string(char_lcd_dev, KEY_STRING);
+			  alt_up_character_lcd_string(char_lcd_dev, key_number);
+		  } else {
+			  alt_up_character_lcd_set_cursor_pos(char_lcd_dev,0,0);
+			  alt_up_character_lcd_string(char_lcd_dev, KEY_STRING_EMPTY);
 		  }
 	  }
-	  *LEDS = number_of_button_presses;
+
+	  button_previously_held_down = button_held_down;
   }
 
   return 0;
