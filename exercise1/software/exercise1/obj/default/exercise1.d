3641f0d7c3fd6a97f7a5b61ddb67dd1b61c9078f
Nicholas Mulvenna <mulvenna@linux.com>
2016-01-26 16:29:54 -0800

    changes to e1_1 and e1_3 from demos

--- exercise1/software/exercise1/obj/default/exercise1.d
+++ exercise1/software/exercise1/obj/default/exercise1.d
@@ -1,1 +1,38 @@
-obj/default/exercise1.o: exercise1.c
+obj/default/exercise1.o: exercise1.c \
+  ../exercise1_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../exercise1_bsp//HAL/inc/sys/alt_dev.h ../exercise1_bsp/system.h \
+  ../exercise1_bsp/linker.h ../exercise1_bsp//HAL/inc/sys/alt_llist.h \
+  ../exercise1_bsp//HAL/inc/alt_types.h \
+  ../exercise1_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../exercise1_bsp//HAL/inc/sys/alt_llist.h \
+  ../exercise1_bsp//HAL/inc/alt_types.h \
+  ../exercise1_bsp//HAL/inc/sys/alt_alarm.h \
+  ../exercise1_bsp//HAL/inc/sys/alt_llist.h \
+  ../exercise1_bsp//HAL/inc/priv/alt_alarm.h \
+  ../exercise1_bsp//HAL/inc/sys/alt_warning.h
+
+../exercise1_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_dev.h:
+
+../exercise1_bsp/system.h:
+
+../exercise1_bsp/linker.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_llist.h:
+
+../exercise1_bsp//HAL/inc/alt_types.h:
+
+../exercise1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_llist.h:
+
+../exercise1_bsp//HAL/inc/alt_types.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_alarm.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_llist.h:
+
+../exercise1_bsp//HAL/inc/priv/alt_alarm.h:
+
+../exercise1_bsp//HAL/inc/sys/alt_warning.h:
