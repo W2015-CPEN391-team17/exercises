c3469588f5bb756a3b645486ca95892c51f81b2a
Timothy Choy <timothychoy@Timothys-MacBook-Air.local>
2016-02-02 10:49:25 -0800

    commit for finalized ex. 1.5 code and project

---
+++ exercise1_5/NIOS_II_System_391_For_Students/software/gps_control_new/obj/default/gps_control.d
@@ -0,0 +1,39 @@
+obj/default/gps_control.o: gps_control.c \
+  ../gps_control_new_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_dev.h \
+  ../gps_control_new_bsp/system.h ../gps_control_new_bsp/linker.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_llist.h \
+  ../gps_control_new_bsp//HAL/inc/alt_types.h \
+  ../gps_control_new_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_llist.h \
+  ../gps_control_new_bsp//HAL/inc/alt_types.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_alarm.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_llist.h \
+  ../gps_control_new_bsp//HAL/inc/priv/alt_alarm.h \
+  ../gps_control_new_bsp//HAL/inc/sys/alt_warning.h
+
+../gps_control_new_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_dev.h:
+
+../gps_control_new_bsp/system.h:
+
+../gps_control_new_bsp/linker.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_llist.h:
+
+../gps_control_new_bsp//HAL/inc/alt_types.h:
+
+../gps_control_new_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_llist.h:
+
+../gps_control_new_bsp//HAL/inc/alt_types.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_alarm.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_llist.h:
+
+../gps_control_new_bsp//HAL/inc/priv/alt_alarm.h:
+
+../gps_control_new_bsp//HAL/inc/sys/alt_warning.h:
