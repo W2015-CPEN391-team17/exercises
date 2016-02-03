cae2aa90e23a42fe4cc6f74aecbe1cf6e020e6a3
Nathan Yee <ny.nathan.yee@gmail.com>
2016-02-02 16:37:00 -0800

    Add Exercise 1.8

---
+++ exercise1_8/NIOS_II_System_391_For_Students/software/cpen_391_exercise_1_8/obj/default/SDCard_Test_Program.d
@@ -0,0 +1,25 @@
+obj/default/SDCard_Test_Program.o: SDCard_Test_Program.c \
+  ../cpen_391_exercise_1_8_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h \
+  ../cpen_391_exercise_1_8_bsp//HAL/inc/alt_types.h \
+  ../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_dev.h \
+  ../cpen_391_exercise_1_8_bsp/system.h \
+  ../cpen_391_exercise_1_8_bsp/linker.h \
+  ../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_llist.h \
+  ../cpen_391_exercise_1_8_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_llist.h
+
+../cpen_391_exercise_1_8_bsp//drivers/inc/altera_up_sd_card_avalon_interface.h:
+
+../cpen_391_exercise_1_8_bsp//HAL/inc/alt_types.h:
+
+../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_dev.h:
+
+../cpen_391_exercise_1_8_bsp/system.h:
+
+../cpen_391_exercise_1_8_bsp/linker.h:
+
+../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_llist.h:
+
+../cpen_391_exercise_1_8_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../cpen_391_exercise_1_8_bsp//HAL/inc/sys/alt_llist.h:
