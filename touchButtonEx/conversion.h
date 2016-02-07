e3259995ff87352736a9f9207b7383b4f1652156
kcamp34 <kylecampbell34@gmail.com>
2016-02-06 16:11:56 -0800

    Conversion function - only for testing.

---
+++ touchButtonEx/conversion.h
@@ -0,0 +1,17 @@
+/*
+ * conversion.h
+ *
+ *  Created on: 2016-02-06
+ *      Author: Kyle
+ */
+
+#ifndef CONVERSION_H_
+#define CONVERSION_H_
+
+#define INT_MIN (-2147483647 - 1)
+#define INT_MAX 2147483647
+
+void convertGPSPointsToPoints( struct Point points[] );
+
+
+#endif /* CONVERSION_H_ */
